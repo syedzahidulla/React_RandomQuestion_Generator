578285a742ea1191dba21d7e0615aca454a0284c
syedzahidulla <szyaehdid@gmail.com>
2020-07-08 20:54:37 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -40,9 +40,8 @@ export class App extends Component {
         
         <Question id={this.id}/>
         <QuestionImage id={this.id} />
-        <Recording recordingHandler={this.updateHandler} isRecording={this.state.isRecording} blob={this.state.blob} />
         <Answer value={this.state.answer} answerHandler={this.updateHandler} />
-        
+        <Recording recordingHandler={this.updateHandler} isRecording={this.state.isRecording} blob={this.state.blob} />
         <div >
           <button className="submitFrame" onClick={this.updateSubmit} disabled={this.state.isRecording}>Submit </button>
         </div>
